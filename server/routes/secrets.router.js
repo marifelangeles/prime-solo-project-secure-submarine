179ea368e4d5969636d2292f039c33f697f9a9dc
Marifel Angeles <mmarifel@gmail.com>
2019-03-13 16:20:29 -0500

    No Secrets Above Clearance Level

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -5,7 +5,8 @@ const router = express.Router();
 
 router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+    pool.query(`SELECT * FROM "secret" 
+            WHERE "secret"."secrecy_level" <=`, [req.user.clearance_level])
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
